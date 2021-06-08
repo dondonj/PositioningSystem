ee81ff23821057ae02649316c9017782a2336b0b
Gabryss <zeratul312@gmail.com>
2021-06-08 20:14:34 +0200

    Add rssi and start_calibration endpoint

--- lab/server/server.py
+++ lab/server/server.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request
+from flask import request, session
 
 from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,19 +7,22 @@ from sqlalchemy.orm import relationship, sessionmaker
 
 import time
 
-app = Flask(__name__)
+from sqlalchemy.sql.expression import false
 
-engine = create_engine('sqlite:///rssi.db', echo=True)
+SECRET_KEY = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
+
+app = Flask(__name__)
+app.config['SECRET_KEY']= SECRET_KEY
+
+engine = create_engine('sqlite:///server/rssi.db', echo=True)
 base = declarative_base()
 base.metadata.create_all(engine)
 
-# app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rssi.db'
-# db = SQLAlchemy(app)
-
-
 Session = sessionmaker(bind=engine)
-session = Session()
+session_ = Session()
+
+# startCalibration=False
+# session["startCalibration"]=startCalibration
 
 class AccessPoint(base):
     __tablename__ = "accesspoint"
@@ -80,33 +83,70 @@ def rssi():
 
     if request.method == 'POST':
         ap = request.args.get('ap')
-        source_adress = request.args.get('source_adress')
+        source_address = request.args.get('source_address')
         rssi = request.args.get('rssi')
-        print(ap + "\n")
-        print(source_adress + "\n")
-        print(rssi + "\n")
 
+        #Check parameters
+        if not ap:
+            return "No Access point parameters"
+        if not source_address:
+            return "No source adress parameters"
+        if not rssi:
+            return "No rssi parameters"
+        
+        #Convert data into the right type
+        ap = str(ap)
+        source_address = str(source_address)
+        rssi = float(rssi)
+
+        #Create an access point
         ap_id = AccessPoint(mac_address=ap)
-        rssi_avrg = Sample(ap_id=ap_id,source_address=source_adress , timestamp=time.time(),rssi=rssi )
+        session_.add(ap_id)
+        session_.flush()
 
-        session.add(ap_id)
-        session.add(rssi_avrg)
+        #Create a Sample
+        rssi_avrg = Sample(ap_id=ap_id.id,source_address=source_address , timestamp=time.time(),rssi=rssi)
 
-        session.commit()
-        session.close()
-        return "POST ok"
+        
+        session_.add(rssi_avrg)
+
+        startCalibration = session.get("startCalibration",None)
+
+        # startCalibration=False
+        #Calibration part
+        if startCalibration:
+            mac_addr = session.get("mac_addr", None)
+            if source_address == mac_addr:
+
+                #Fetch the loc.id of the mac_addr in the CalibratingMobile table
+                for loc in session_.query(CalibratingMobile).order_by(CalibratingMobile.mac_address):
+                    if loc.mac_address == mac_addr:
+                        location = loc.loc_id
+
+                #Create a fingerprint
+                fingerprint = FingerprintValue(loc_id=location , ap_id=ap_id.id, rssi=rssi)
+                session_.add(fingerprint)
+
+        #Commit and close   
+        session_.commit()
+        session_.close()
+        return "\n\rPOST ok\n"
     else:
-        print("coucou")
-        for instance in session.query(Sample).order_by(Sample.rssi):
-            print(instance.ap_id, instance.rssi)
-        return "GET ok"
+        print("\n Sample table \n")
+        for instance in session_.query(Sample).order_by(Sample.rssi):
+            print(instance.source_address, instance.ap_id, instance.timestamp, instance.rssi)
+
+        print("\n AccessPoint table \n")
+        for instance in session_.query(AccessPoint).order_by(AccessPoint.id):
+            print(instance.id, instance.mac_address)
+        return "\n\rGET ok\n"
 
 
 
 @app.route("/start_calibration", methods=['GET', 'POST'])
 def start_calibration():
     """
-        TODO: implement this function
+        ToDO: implement this function
         It receives 4 parameters: mac_addr (string), x (float), y (float), and z (float)
         then must trigger 3 tasks:
         (1) Add MAC address and location to table calibrating_mobile
@@ -119,11 +159,72 @@ def start_calibration():
             step (2) when received.
     """
     if request.method == 'POST':
-        data = request.data
+        mac_addr = request.args.get('mac_addr')
+        session["mac_addr"] = mac_addr              #Share mac address between routes
+        x = request.args.get('x')
+        y = request.args.get('y')
+        z = request.args.get('z')
+
+        #Check parameters
+        if not mac_addr:
+            return "No mac address parameters provided"
+        if not x:
+            return "No x parameters provided"
+        if not y:
+            return "No y parameters provided"
+        if not z:
+            return "No z parameters prodided"
+        
+        #Convert data into the right type
+        mac_addr = str(mac_addr)
+        x = float(x)
+        y = float(y)
+        z = float(z)
+
+        #Fill callibrating_mobile table
+        location = Location(x=x, y=y, z=z)
+        session_.add(location)
+        session_.flush()
+
+        cal_mobile = CalibratingMobile(mac_address=mac_addr, loc_id=location.id)
+        session_.add(cal_mobile)
+
+        sampleList=[]
+        for instance in session_.query(Sample).order_by(Sample.source_address):
+            if instance.source_address == mac_addr and instance.timestamp >= time.time()-1:
+                
+                #Share start calibration variable
+                if startCalibration == false:
+                    startCalibration=True
+                    session["startCalibration"]=startCalibration 
+
+                #Fill list of samples
+                sampleList.add(instance)
+        
+        if len(sampleList)>0:
+            for sample in sampleList:
+                fingerprint = FingerprintValue(loc_id = location.id, ap_id = sample.ap_id, rssi=sample.rssi)
+                session_.add(fingerprint)
+
+        #Commit and close
+        session_.commit()
+        session_.close()
+        return "\n\rPOST ok\n"
 
     else:
+        print("\n Location table \n")
+        for instance in session_.query(Location).order_by(Location.id):
+            print(instance.id, instance.x, instance.y, instance.z)
 
-        return "ok"
+        print("\n Calibrating mobile table \n")
+        for instance in session_.query(CalibratingMobile).order_by(CalibratingMobile.mac_address):
+            print(instance.mac_address, instance.loc_id)
+        
+        print("\n Fingerprint value table \n")
+        for instance in session_.query(FingerprintValue).order_by(FingerprintValue.id):
+            print(instance.id, instance.loc_id, instance.ap_id, instance.rssi)
+
+        return "\n\rGET ok\n"
 
 
 @app.route("/stop_calibration", methods=['GET', 'POST'])
