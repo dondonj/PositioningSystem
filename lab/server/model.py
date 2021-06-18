6784125099ad4e209b31c58228acfed1d61be674
Gabryss <zeratul312@gmail.com>
2021-06-18 18:40:59 +0200

    Add database model

---
+++ lab/server/model.py
@@ -0,0 +1,47 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+base = declarative_base()
+
+class AccessPoint(base):
+    __tablename__ = "accesspoint"
+    id = Column(Integer, primary_key=True)
+    mac_address = Column(String)
+
+class Location(base):
+    __tablename__ = "location"
+    id = Column(Integer, primary_key=True)
+    x = Column(Float)
+    y = Column(Float)
+    z = Column(Float)
+
+class Sample(base):
+    __tablename__ = "sample"
+    ap_id = Column(Integer, ForeignKey("accesspoint.id"))
+    source_address = Column(String, nullable=False, primary_key=True)
+    timestamp = Column(Float, nullable=False, primary_key=True)
+    rssi = Column(Float, nullable=False)
+    ap = relationship("AccessPoint", backref="sample")
+
+    def values(self, src, t, _rssi, _ap):
+        source_address = src
+        timestamp = t
+        rssi = _rssi
+        ap = _ap
+
+class FingerprintValue(base):
+    __tablename__ = "fingerprint_value"
+    id = Column(Integer, primary_key=True)
+    loc_id = Column(Integer, ForeignKey("location.id"))
+    ap_id = Column(Integer, ForeignKey("accesspoint.id"))
+    rssi = Column(Float, nullable=False)
+    location = relationship("Location", backref="fingerprint_value")
+    ap = relationship("AccessPoint", backref="fingerprint_value")
+
+class CalibratingMobile(base):
+    __tablename__ = "calibrating_mobile"
+    mac_address = Column(String, primary_key=True)
+    loc_id = Column(Integer, ForeignKey("location.id"))
+    location = relationship("Location", backref="calibrating_mobile")
